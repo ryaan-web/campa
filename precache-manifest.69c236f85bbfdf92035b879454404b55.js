0090f9283e4042164142223ff60a6edcf6e782da
Abhishek Kumar <abhishek.kumar@AbhishekKumar.local>
2019-09-20 16:07:06 +0530

    Updates

---
+++ precache-manifest.69c236f85bbfdf92035b879454404b55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11674b8a880745a1c42a15ddb300e7fa",
+    "url": "/campa/index.html"
+  },
+  {
+    "revision": "3c4fbc43091422d5cffd",
+    "url": "/campa/static/css/2.ff9ea1f9.chunk.css"
+  },
+  {
+    "revision": "393c8deecfe0629775ab",
+    "url": "/campa/static/css/main.f10f0d2a.chunk.css"
+  },
+  {
+    "revision": "3c4fbc43091422d5cffd",
+    "url": "/campa/static/js/2.a89466b3.chunk.js"
+  },
+  {
+    "revision": "393c8deecfe0629775ab",
+    "url": "/campa/static/js/main.531efdd6.chunk.js"
+  },
+  {
+    "revision": "514ce2ce752bd155f7ee",
+    "url": "/campa/static/js/runtime-main.d522148f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/campa/static/media/logo.25bf045c.svg"
+  }
+]);
